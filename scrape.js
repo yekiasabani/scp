acfc37bfff0a24738143456d4a2ef581c3e9547d
Esmaeil Keshtkar <Esmaeil@Esmaeils-MacBook-Pro.local>
2024-10-21 00:11:11 +0400

    Updated

--- scrape.js
+++ scrape.js
@@ -1,6 +1,16 @@
 import puppeteer from 'puppeteer';
 const rate = async function getRate(res, currencies) {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({
+        args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote"
+        ],
+        executablePath: process.env.NODE_ENV === 'production' 
+        ? process.env.PUPPETEER_EXECUTABLE_PATH 
+        : puppeteer.executablePath()
+    });
     try {
         const page = await browser.newPage();
         await page.goto('https://bonbast.com/');
